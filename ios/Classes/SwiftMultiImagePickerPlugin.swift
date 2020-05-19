7629a78a35fdd726ef834a07d2bbdf02c473b6ce
johnson.zhong <johnson.zhong@idreamsky.com>
2020-05-19 11:38:17 +0800

    增加缩略图

--- ios/Classes/SwiftMultiImagePickerPlugin.swift
+++ ios/Classes/SwiftMultiImagePickerPlugin.swift
@@ -91,7 +91,7 @@ public class SwiftMultiImagePickerPlugin: NSObject, FlutterPlugin {
             if let selectionCharacter = options["selectionCharacter"] , !selectionCharacter.isEmpty {
                 vc.selectionCharacter = Character(selectionCharacter)
             }
-            
+
             let thumbDir = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last ?? NSTemporaryDirectory()) + "/multi_image_pick/thumb/"
             let manager = PHImageManager.default()
             let thumbOptions = PHImageRequestOptions()
@@ -125,32 +125,73 @@ public class SwiftMultiImagePickerPlugin: NSObject, FlutterPlugin {
                             options.isNetworkAccessAllowed = false
                             manager.requestAVAsset(forVideo: asset, options: options) { (avAsset, audioMix, info) in
                                 count += 1;
-                                let uuid = UUID().uuidString
-                                let fileName = "\(uuid).mp4"
-                                let filePath = thumbDir + fileName
-                                if FileManager.default.fileExists(atPath: filePath) {
-                                    try? FileManager.default.removeItem(atPath: filePath)
-                                }
                                 if avAsset != nil {
+                                    let dictionary : NSMutableDictionary = NSMutableDictionary()
+                                    let uuid = UUID().uuidString
+                                    let thumbName = "\(uuid).jpg"
+                                    let videoName = "\(uuid).mp4"
+                                    let thumbPath = thumbDir + thumbName
+                                    let videoPath = thumbDir + videoName
+                                    if FileManager.default.fileExists(atPath: videoPath) {
+                                        try? FileManager.default.removeItem(atPath: videoPath)
+                                    }
+                                    if FileManager.default.fileExists(atPath: thumbPath) {
+                                        try? FileManager.default.removeItem(atPath: thumbPath)
+                                    }
+                                    
+                                    let gen = AVAssetImageGenerator(asset: avAsset!)
+                                    gen.appliesPreferredTrackTransform = true
+                                    let time = CMTimeMakeWithSeconds(2.0, preferredTimescale: 600);
+                                    var actualTime  = CMTimeMake(value: 0, timescale: 0)
+                                    if let image = try? gen.copyCGImage(at: time, actualTime: &actualTime) {
+                                        let thumbImg = UIImage(cgImage: image)
+                                        do {
+                                            try thumbImg.jpegData(compressionQuality: 1.0)?.write(to: URL(fileURLWithPath: thumbPath))
+                                        } catch let error as NSError {
+                                            print(error)
+                                            if (count >= assets.count) {
+                                                result(results);
+                                            }
+                                        }
+                                        dictionary.setValue(thumbPath, forKey: "thumbPath")
+                                        dictionary.setValue(thumbImg.size.height * thumbImg.scale, forKey: "thumbHeight")
+                                        dictionary.setValue(thumbImg.size.width * thumbImg.scale, forKey: "thumbWidth")
+                                    }else {
+                                        if (count >= assets.count) {
+                                            result(results);
+                                        }
+                                    }
+                                    
                                     let exportSession = AVAssetExportSession(asset: avAsset!, presetName: AVAssetExportPresetMediumQuality)
-                                    exportSession?.outputURL = URL(fileURLWithPath: filePath)
+                                    exportSession?.outputURL = URL(fileURLWithPath: videoPath)
                                     exportSession?.shouldOptimizeForNetworkUse = true
                                     exportSession?.outputFileType = .mp4
                                     exportSession?.exportAsynchronously(completionHandler: {
-                                        if FileManager.default.fileExists(atPath: filePath) {
-                                            results.append([
-                                                "identifier": asset.localIdentifier,
-                                                "filePath":filePath,
-                                                "width": 0.0,
-                                                "height": 0.0,
-                                                "name": fileName,
-                                                "fileType":"video"
-                                            ]);
+                                        if FileManager.default.fileExists(atPath: videoPath) {
+                                            let thumbVideo = AVURLAsset(url: URL(fileURLWithPath: videoPath))
+                                            var thumbVideoSize = CGSize.zero
+                                            for track in thumbVideo.tracks {
+                                                if track.mediaType == AVMediaType.video {
+                                                    thumbVideoSize = track.naturalSize
+                                                }
+                                            }
+                                            
+                                            dictionary.setValue(asset.localIdentifier, forKey: "identifier")
+                                            dictionary.setValue(videoPath, forKey: "filePath")
+                                            dictionary.setValue(thumbVideoSize.width, forKey: "width")
+                                            dictionary.setValue(thumbVideoSize.height, forKey: "height")
+                                            dictionary.setValue(videoName, forKey: "name")
+                                            dictionary.setValue("video", forKey: "fileType")
+                                            results.append(dictionary);
                                         }
                                         if (count >= assets.count) {
                                             result(results);
                                         }
                                     })
+                                }else {
+                                    if (count >= assets.count) {
+                                        result(results);
+                                    }
                                 }
                             }
                         }else {
@@ -192,7 +233,11 @@ public class SwiftMultiImagePickerPlugin: NSObject, FlutterPlugin {
                                 }
                             })
                             if(PHInvalidImageRequestID != ID) {
-                                continue
+                                if (count >= assets.count) {
+                                    result(results);
+                                }else {
+                                    continue
+                                }
                             }
                         }
                     }

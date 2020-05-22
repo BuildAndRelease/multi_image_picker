f00a60af19c641003d3ba77bab2d7db01b06760a
johnson.zhong <johnson.zhong@idreamsky.com>
2020-05-22 20:14:22 +0800

    修改名称错误

--- android/src/main/java/com/sangcomz/fishbun/ui/picker/PickerActivity.java
+++ android/src/main/java/com/sangcomz/fishbun/ui/picker/PickerActivity.java
@@ -366,7 +366,7 @@ public class PickerActivity extends BaseActivity implements View.OnClickListener
                         String uuid = UUID.randomUUID().toString();
                         String videoName = uuid + ".mp4";
                         String imgName = uuid + ".jpg";
-                        String cacheDir = getExternalCacheDir().getAbsolutePath() + "/muti_image_pick/thumb/";
+                        String cacheDir = getExternalCacheDir().getAbsolutePath() + "/multi_image_pick/thumb/";
                         File tmpPicParentDir = new File(cacheDir);
                         if (!tmpPicParentDir.exists()) {
                             tmpPicParentDir.mkdirs();

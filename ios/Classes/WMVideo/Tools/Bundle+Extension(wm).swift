fae26b5b3025154231c90295a87a3411c84b1a0e
johnson.zhong <johnson.zhong@idreamsky.com>
2020-07-06 11:27:40 +0800

    拿掉多余图片库

--- ios/Classes/WMVideo/Tools/Bundle+Extension(wm).swift
+++ ios/Classes/WMVideo/Tools/Bundle+Extension(wm).swift
@@ -14,9 +14,6 @@ extension Bundle{
     ///
     /// - Returns: Bundle
     class func wm_videoBundle() -> Bundle{
-//        var bundle:Bundle = Bundle.init(for: WMCameraViewController.self)
-//        let url:URL = bundle.url(forResource: "WMCameraResource", withExtension: "bundle")!
-//        bundle = Bundle.init(url: url)!
         return Bundle.init(for: WMCameraViewController.self)
     }
     

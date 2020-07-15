2b2ff4ecc1255dd0e32f2a6bee107d4cc6fc3c8c
johnson.zhong <johnson.zhong@idreamsky.com>
2020-07-15 20:37:03 +0800

    拿掉日志打印

--- ios/Classes/WMVideo/WMCameraViewController.swift
+++ ios/Classes/WMVideo/WMCameraViewController.swift
@@ -111,18 +111,16 @@ class WMCameraViewController: UIViewController {
     }
 
     open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesBegan \(touches)")
-     }
+
+    }
      
      open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesMoved \(touches)")
-     }
+
+    }
      
      open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesEnded \(touches)")
-     }
 
-    
+    }
 }
 
 extension WMCameraViewController: WMCameraControlDelegate {

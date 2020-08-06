be83ca592cf83f67f553d19489efc7eaaad67210
johnson.zhong <johnson.zhong@idreamsky.com>
2020-08-06 20:56:39 +0800

    增加视频缩略图字段

--- android/src/main/java/com/sangcomz/fishbun/util/MediaInfoData.java
+++ android/src/main/java/com/sangcomz/fishbun/util/MediaInfoData.java
@@ -8,7 +8,12 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 
+import com.sangcomz.fishbun.bean.Media;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class MediaInfoData extends AsyncTask<Void, Void, HashMap> {
     public interface MediaInfoDataListener {
@@ -34,6 +39,7 @@ public class MediaInfoData extends AsyncTask<Void, Void, HashMap> {
         String mimeType = "";
         String width = "0";
         String height = "0";
+        String identify = "";
         try {
             Uri uri = MediaStore.Files.getContentUri("external");
             String selection = MediaStore.MediaColumns._ID + "=" + identify;
@@ -41,6 +47,7 @@ public class MediaInfoData extends AsyncTask<Void, Void, HashMap> {
             if (c != null) {
                 try {
                     if (c.moveToFirst()) {
+                        identify = c.getString(c.getColumnIndex(MediaStore.MediaColumns._ID));
                         size = c.getString(c.getColumnIndex(MediaStore.MediaColumns.SIZE));
                         filePath = c.getString(c.getColumnIndex(MediaStore.MediaColumns.DATA));
                         mimeType = c.getString(c.getColumnIndex(MediaStore.MediaColumns.MIME_TYPE));
@@ -59,16 +66,55 @@ public class MediaInfoData extends AsyncTask<Void, Void, HashMap> {
         HashMap hashMap = new HashMap();
         hashMap.put("size", size);
         if (mimeType.contains("video")) {
-            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-            retriever.setDataSource(filePath);
-            Bitmap bitmap = retriever.getFrameAtTime(0);
-            width = bitmap.getWidth() + "";
-            height = bitmap.getHeight() + "";
-            retriever.release();
+            String uuid = UUID.randomUUID().toString();
+            String imgName = uuid + ".jpg";
+            String cacheDir = context.getCacheDir().getAbsolutePath() + "/multi_image_pick/thumb/";
+            File tmpPicParentDir = new File(cacheDir);
+            if (!tmpPicParentDir.exists()) {
+                tmpPicParentDir.mkdirs();
+            }
+            File tmpPic = new File(cacheDir + imgName);
+            if (tmpPic.exists()) {
+                tmpPic.delete();
+            }
+            HashMap t = localVideoThumb(filePath, tmpPic.getAbsolutePath());
+            width = (String) t.get("width");
+            height = (String) t.get("height");
+            hashMap.put("fileType", "video");
+            hashMap.put("thumbPath", tmpPic.getAbsolutePath());
+            hashMap.put("thumbName", imgName);
+            hashMap.put("thumbHeight", width);
+            hashMap.put("thumbWidth", height);
         }
         hashMap.put("width", width);
         hashMap.put("height", height);
+        hashMap.put("identifier", identify);
         return hashMap;
+    }
+
+    public HashMap<String, String> localVideoThumb(String filePath, String savePath) {
+        HashMap result = new HashMap();
+        Bitmap bitmap = null;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(filePath);
+            bitmap = retriever.getFrameAtTime(0);
+            File f = new File(savePath);
+            FileOutputStream out = new FileOutputStream(f);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            result.put("width", (bitmap.getWidth() * 1.0) + "");
+            result.put("height", (bitmap.getHeight() * 1.0) + "");
+            out.flush();
+            out.close();
+            if(!bitmap.isRecycled()){
+                bitmap.recycle();//记得释放资源，否则会内存溢出
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            retriever.release();
+        }
+        return result;
     }
 
     @Override

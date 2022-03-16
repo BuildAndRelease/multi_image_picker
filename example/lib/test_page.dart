6c4c888736057f47a16749f6f4cefc05f1cd725d
zhoulin <lin1.zhou@idreamsky.com>
2022-03-16 10:48:59 +0800

    add test page

---
+++ example/lib/test_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatefulWidget {
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test page"),
+      ),
+      body: Container(),
+    );
+  }
+}
